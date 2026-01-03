db539fba6cc9f2a7c3e5cc88d0f9e5d71f11bedc
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-01-03 22:03:21 +0530

    Internationalization

---
+++ IOCProj17-Enabling-I18n/src/main/java/com/nt/test/I18n_Test.java
@@ -0,0 +1,35 @@
+package com.nt.test;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class I18n_Test {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in);
+				         //Creating the IOC Container
+				          ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");){
+			//Taking input from the end user
+			System.out.println("Enter the lang Code : ");
+			String lang = sc.next();
+			System.out.println("Enter the country code : ");
+			String country = sc.next();
+			
+			//Creating the Locale object with above data
+			Locale locale = new Locale(lang, country);
+			
+			String msg1 = ctx.getMessage("greet.msg", new Object[] {}, locale);
+			String msg2 = ctx.getMessage("bye.msg", new Object[] {}, locale);
+			
+			System.out.println(msg1);
+			System.out.println(msg2);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}//main
+
+}//class
